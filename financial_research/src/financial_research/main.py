2ce71697c4a8430e990e6045260c5475bd0eb124
Muhammad Ariff Izzham Bin Burhanuddin <ARIFF.BURHANUDDIN@TMRND.COM.MY>
2025-08-17 00:15:06 +0800

    add FinancialResearch

--- financial_research/src/financial_research/main.py
+++ financial_research/src/financial_research/main.py
@@ -18,9 +18,11 @@ def run():
     Run the crew.
     """
     inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
+        'title': 'Tesla',
     }
+    
+    result= FinancialResearch().crew().kickoff(inputs=inputs)
+    print(result.raw)
     
     try:
         FinancialResearch().crew().kickoff(inputs=inputs)
@@ -28,41 +30,7 @@ def run():
         raise Exception(f"An error occurred while running the crew: {e}")
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        'current_year': str(datetime.now().year)
-    }
-    try:
-        FinancialResearch().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
+if __name__ == "__main__" :
+    run()
 
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        FinancialResearch().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs",
-        "current_year": str(datetime.now().year)
-    }
-    
-    try:
-        FinancialResearch().crew().test(n_iterations=int(sys.argv[1]), eval_llm=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
